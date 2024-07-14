9cbff862b3d5bdaaeb2788b745cf35ea9356705c
Un1q32 <joey.t.reinhart@gmail.com>
2024-07-13 23:16:54 -0400

    fix pipe

--- src/sys/pipe.c
+++ src/sys/pipe.c
@@ -1,4 +1,12 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-int pipe(int pipefd[2]) { return syscall(SYS_pipe, pipefd); }
+int pipe(int pipefd[2]) {
+  int pipefd1 = syscall(SYS_pipe);
+  if (pipefd1 == -1)
+    return -1;
+  int pipefd2 = __syscallret2;
+  pipefd[0] = pipefd1;
+  pipefd[1] = pipefd2;
+  return 0;
+}
