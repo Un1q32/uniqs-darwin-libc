720bd9b47b79b78480ee0b79fd9b2cb5d658914e
Un1q32 <joey.t.reinhart@gmail.com>
2024-07-21 15:18:27 -0400

    update kill syscall, specify posix conformance

--- src/sys/kill.c
+++ src/sys/kill.c
@@ -1,4 +1,4 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-int kill(pid_t pid, int sig) { return syscall(SYS_kill, pid, sig); }
+int kill(pid_t pid, int sig) { return syscall(SYS_kill, pid, sig, 1); }
