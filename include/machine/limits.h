039b8ab8b0c42690ecf6f3701322987c4d28a649
Un1q32 <joey.t.reinhart@gmail.com>
2024-07-22 01:45:31 -0400

    change define

--- include/machine/limits.h
+++ include/machine/limits.h
@@ -1,7 +1,7 @@
 #ifndef _MACHINE_LIMITS_H_
 #define _MACHINE_LIMITS_H_
 
-#if defined(__arm__) || defined(__arm64__)
+#if defined(__arm__) || defined(__aarch64__)
 #include <machine/arm/limits.h>
 #elif defined(__i386__) || defined(__x86_64__)
 #include <machine/x86/limits.h>
--- include/machine/param.h
+++ include/machine/param.h
@@ -1,7 +1,7 @@
 #ifndef _MACHINE_PARAM_H_
 #define _MACHINE_PARAM_H_
 
-#if defined(__arm__) || defined(__arm64__)
+#if defined(__arm__) || defined(__aarch64__)
 #include <machine/arm/param.h>
 #elif defined(__i386__) || defined(__x86_64__)
 #include <machine/x86/param.h>
