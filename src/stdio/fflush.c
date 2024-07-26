9d8dbfaadc8821abfa21695625033e58e5469ea2
Un1q32 <joey.t.reinhart@gmail.com>
2024-07-26 02:43:49 -0400

    fix

--- src/stdio/fflush.c
+++ src/stdio/fflush.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 int fflush(FILE *stream) {
-  ssize_t writeret = write(stream->fd, stream->buf, stream->bufcount);
+  ssize_t writeret = stream->write(stream->fd, stream->buf, stream->bufcount);
   if (writeret == -1)
     return EOF;
   stream->bufcount = 0;
